fa525838da4e9e8cd74bd98523691dd49a459baf
Dafna101 <da.kuka@mail.ru>
2019-02-25 14:46:31 +0100

    dodano icons do nawigacji. Przeniesiono search-form do oddzielnego komponentu w shared/components/

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { LoaderComponent } from './components/loader/loader.component';
+import { SearchFormComponent } from './components/search-form/search-form.component';
+
 
 @NgModule({
-  declarations: [LoaderComponent],
+  declarations: [LoaderComponent, SearchFormComponent],
   imports: [
     CommonModule
+  ],
+  exports: [
+    SearchFormComponent
   ]
 })
 export class SharedModule { }
