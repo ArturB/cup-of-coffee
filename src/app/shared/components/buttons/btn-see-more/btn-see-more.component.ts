f24aeb4d97011c194f14b22a73915226b6bddd94
Dafna101 <da.kuka@mail.ru>
2019-02-25 22:58:51 +0100

    stworzono btn-see-more

---
+++ src/app/shared/components/buttons/btn-see-more/btn-see-more.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-btn-see-more',
+  templateUrl: './btn-see-more.component.html',
+  styleUrls: ['./btn-see-more.component.css']
+})
+export class BtnSeeMoreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,16 +4,18 @@ import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './components/loader/loader.component';
 import { SearchFormComponent } from './components/search-form/search-form.component';
 import { CollapsedArticleComponent } from './components/collapsed-article/collapsed-article.component';
+import { BtnSeeMoreComponent } from './components/buttons/btn-see-more/btn-see-more.component';
 
 
 @NgModule({
-  declarations: [LoaderComponent, SearchFormComponent, CollapsedArticleComponent],
+  declarations: [LoaderComponent, SearchFormComponent, CollapsedArticleComponent, BtnSeeMoreComponent],
   imports: [
     CommonModule
   ],
   exports: [
     SearchFormComponent,
-    CollapsedArticleComponent
+    CollapsedArticleComponent,
+    BtnSeeMoreComponent
     
   ]
 })
