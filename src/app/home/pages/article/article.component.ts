65a1d65c922b3cf6d4ec293af5b1f9f4cc726368
Dafna101 <da.kuka@mail.ru>
2019-02-27 15:43:41 +0100

    zmodyfikowano styles.css. Dodano zdarzenie onFavorite() do star

--- src/app/home/pages/article/article.component.ts
+++ src/app/home/pages/article/article.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticleComponent implements OnInit {
 
+  isFavorite: boolean = false;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onFavorite() {
+    console.log(this.isFavorite);
+    this.isFavorite = !this.isFavorite;
+    
   }
 
 }
