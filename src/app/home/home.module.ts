177b17d9ab642894ce4b683b22db36a683c42df5
Dafna101 <da.kuka@mail.ru>
2019-03-03 15:23:24 +0100

    Przeniesiono folder collapsed-category do shared/components

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,24 +7,22 @@ import { SharedModule } from '../shared/shared.module';
 
 import { HomeUsComponent } from './pages/home-us/home-us.component';
 import { CategoryComponent } from './pages/category/category.component';
+import { SelectedCategoryComponent } from './pages/category/selected-category/selected-category.component';
 import { ArticleComponent } from './pages/category/article/article.component';
 import { AddArticleComponent } from './pages/add-article/add-article.component';
 import { UserAccountComponent } from './pages/user-account/user-account.component';
 
-import { CollapsedCategoryComponent } from './components/collapsed-category/collapsed-category.component';
-
 import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { SelectedCategoryComponent } from './pages/category/selected-category/selected-category.component';
 
 @NgModule({
   declarations: [
     AddArticleComponent, 
     UserAccountComponent, 
     CategoryComponent, 
+    SelectedCategoryComponent,
     ArticleComponent,
     HomeUsComponent, 
-    CollapsedCategoryComponent, 
-    NotFoundComponent, SelectedCategoryComponent
+    NotFoundComponent, 
   ],
   imports: [
     CommonModule,
@@ -32,7 +30,8 @@ import { SelectedCategoryComponent } from './pages/category/selected-category/se
     SharedModule
   ],
   exports: [
-    CollapsedCategoryComponent
+    // CollapsedCategoryComponent,
+    // CollapsedArticleComponent
   ]
 })
 export class HomeModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { HomeRoutingModule } from '../home/home-routing.module';
+
 import { LoaderComponent } from './components/loader/loader.component';
+import { CollapsedCategoryComponent } from './components/collapsed-category/collapsed-category.component';
 import { CollapsedArticleComponent } from './components/collapsed-article/collapsed-article.component';
 import { BtnSeeMoreComponent } from './components/buttons/btn-see-more/btn-see-more.component';
 
 
 @NgModule({
-  declarations: [LoaderComponent, CollapsedArticleComponent, BtnSeeMoreComponent],
+  declarations: [LoaderComponent, CollapsedCategoryComponent, CollapsedArticleComponent, BtnSeeMoreComponent],
   imports: [
-    CommonModule
+    CommonModule,
+
+    HomeRoutingModule
   ],
   exports: [
+    CollapsedCategoryComponent,
     CollapsedArticleComponent,
     BtnSeeMoreComponent
     
