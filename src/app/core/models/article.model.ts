c32f726ed35deab095768cd83239d745cc31b115
Dafna101 <da.kuka@mail.ru>
2019-03-06 16:12:49 +0100

    Zminiono kolejność pół w article.model

--- src/app/core/models/article.model.ts
+++ src/app/core/models/article.model.ts
@@ -2,9 +2,9 @@ export class Article {
     constructor(public articleId: number,
                 public link: string,
                 public title: string,
+                public category: string,
                 public author: string,
                 public description: string,
-                public category: string,
                 public viewed: number,
                 public dateModified: Date,) {}
 }
--- src/app/home/pages/add-article/add-article.component.ts
+++ src/app/home/pages/add-article/add-article.component.ts
@@ -14,9 +14,9 @@ export class AddArticleComponent implements OnInit {
     articleId: 10,
     link: "./assets/img/cup.png",
     title: "NOWY ARTYKUŁ",
+    category: "popularne",
     author: "Darika",
     description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit rem esse eaque libero quod perspiciatis, tempore suscipit qui veritatis autem eum adipisci dolor quidem reiciendis soluta veniam ipsam nisi vero tempora sequi provident ratione explicabo est? Sint ex eveniet doloremque necessitatibus accusantium deleniti corporis vel ullam fugiat magnam! Ab quo sapiente fuga esse perspiciatis nostrum maxime maiores delectus excepturi velit, qui vel eius est perferendis nam enim totam laborum quis temporibus provident debitis rerum, numquam ea dolores! Tenetur iure libero eaque temporibus ut, rem velit necessitatibus optio, voluptatem quaerat in. Nesciunt atque, voluptatum ratione vero error nam voluptate nisi ad itaque architecto doloremque repudiandae officia in unde dolorum aliquid ullam illo, dolor ipsa excepturi accusamus blanditii`,
-    category: "popularne",
     viewed: 12,
     dateModified: new Date()
   };
