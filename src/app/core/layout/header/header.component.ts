409e63650f491f89bb11b563096378435bdc2827
Dafna101 <da.kuka@mail.ru>
2019-03-08 01:33:51 +0100

    Modyfikacje w header.component szablonie i stylach w celu poprawnego wyświetlenia dropdown Kategorie

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  // category: string;
+  // private parametersObservable: any;
+  // isCategory: boolean = false;
+  constructor(private route: ActivatedRoute, private router: Router) {
+    
+  }
 
   ngOnInit() {
+    // this.parametersObservable = this.route.params.subscribe(params => {
+    //   //"product" is obtained from 'ProductResolver'
+    //   this.category = this.route.snapshot.data['category'];
+    // });
+    this.  router.routeReuseStrategy.shouldReuseRoute = () => false;
+    // console.log('ffff ',this.route.snapshot.params['category']);
+    // console.log('this.category ',this.category);
   }
+  // ngAfterViewChecked() {
+  //   //Called after ngOnInit when the component's or directive's content has been initialized.
+  //   //Add 'implements AfterContentInit' to the class.
+  //   console.log(this.router.url);
+  //   if (this.router.url === '/kategorie/sztuka') {
+  //     this.isCategory =true;
+  //   }
+  //   else {
+  //     this.isCategory = false;
+  //   }
+  // }
+
+  // ngOnDestroy() {
+  //   if(this.parametersObservable != null) {
+  //     this.parametersObservable.unsubscribe();
+  //   }
+  // }
 
 }
