8e161cdf302d4cd423871dd1d86a4c5a9c04a0d7
Dafna101 <da.kuka@mail.ru>
2019-04-05 01:58:12 +0200

    Poprawa niepoprawneho linku fo DB

--- server/config/database.js
+++ server/config/database.js
@@ -3,7 +3,7 @@ const crypto = require('crypto').randomBytes(256).toString('hex');
 module.exports = {
 	// uri: 'mongodb://localhost:27017/CofeeDB', // Databse URI and database name
 	// uri: 'mongodb://cup-of-coffee.documents.azure.com:10255/?ssl=true',
-	uri: 'mongodb://cup-of-coffee.documents.azure.com:10255/?ssl=true',
+	uri: 'mongodb://madperson:apple22@ds046037.mlab.com:46037/cup-of-coffee',
 	secret: crypto
 
 }
